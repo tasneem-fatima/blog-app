0c01e4f53b36ac3f38155330a38d286bd8438bbe
Tasneem Fatima <tasneems2208@gmail.com>
2022-08-05 01:14:53 +0530

    adding react-markdown package

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import reactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 const DUMMY_POST = {
@@ -11,9 +13,9 @@ const DUMMY_POST = {
 }
 const PostContent = () => {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
-  return <article>
+  return <article className={classes.content}>
     <PostHeader title={DUMMY_POST.title} image={imagePath} />
-    {DUMMY_POST.content}
+    <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
   </article>
 };
 
