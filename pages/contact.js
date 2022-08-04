a3a2b3ea6bf9a6d1f8bc1eda27256a5d23d247e5
Tasneem Fatima <tasneems2208@gmail.com>
2022-08-04 22:56:22 +0530

    adding basic code structure

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactsPage = () => {
+  return (
+    <div>
+        <h1>
+            Contacts Page
+        </h1>
+    </div>
+  )
+}
+
+export default ContactsPage
---
+++ pages/posts/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllPostsPage = () => {
+  return (
+    <div>
+      <h1>AllPostsPage</h1>
+    </div>
+  );
+};
+
+export default AllPostsPage;
