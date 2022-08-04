26be89fce254bc7261bf465e9bf0c1cdd7e78e2c
Tasneem Fatima <tasneems2208@gmail.com>
2022-08-05 00:39:56 +0530

    adding all-posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,30 @@
 import React from "react";
-
+import AllPosts from "../../components/posts/all-posts";
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a framework for production",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs1",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a framework for production",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with Nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt: "NextJS is a framework for production",
+    date: "2022-02-10",
+  },
+];
 const AllPostsPage = () => {
-  return (
-    <div>
-      <h1>AllPostsPage</h1>
-    </div>
-  );
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
 export default AllPostsPage;
