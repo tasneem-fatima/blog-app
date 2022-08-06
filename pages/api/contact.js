b7fcfc8485a2d0dc706374f9d015a9c3e4cb9849
Tasneem Fatima <74507252+tasneem-fatima@users.noreply.github.com>
2022-08-06 20:51:11 +0530

    added mongodb connection

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb'
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -21,6 +21,25 @@ export default function handler(req, res) {
       message,
     };
     console.log(newMessage, "New Message");
+
+    let client;
+    try {
+      client = await MongoClient.connect('mongodb+srv://tasneemH:tasneemWazir@cluster0.b2cjx7t.mongodb.net/?retryWrites=true&w=majority');
+
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database' });
+      return;
+    }
+    const db = client.db();
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing mssages' });
+      return;
+    }
+    client.close();
 
     res
       .status(201)
